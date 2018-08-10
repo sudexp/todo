ad554361677f27a074b6acb1e8ebf808923f63ce
sudexp <sudexp@icloud.com>
2018-08-10 11:52:21 +0300

    todolist formatted

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -68,185 +68,184 @@
 
 <script>
 export default {
-  name: 'todo-list',
-  data () {
+  name: "todo-list",
+  data() {
     return {
-      newTodo: '',
+      newTodo: "",
       idForTodo: 3,
-      beforeEditCache: '',
-      filter: 'all',
+      beforeEditCache: "",
+      filter: "all",
       todos: [
         {
-          'id': 1,
-          'title': 'Task 1',
-          'completed': false,
-          'editing': false
+          id: 1,
+          title: "Task 1",
+          completed: false,
+          editing: false
         },
         {
-          'id': 2,
-          'title': 'Task 2',
-          'completed': false,
-          'editing': false
+          id: 2,
+          title: "Task 2",
+          completed: false,
+          editing: false
         }
       ]
-    }
+    };
   },
   computed: {
     remaining() {
-      return this.todos.filter(todo => !todo.completed).length
+      return this.todos.filter(todo => !todo.completed).length;
     },
     anyRemaining() {
-      return this.remaining !== 0
+      return this.remaining !== 0;
     },
     todosFiltered() {
-      if(this.filter === 'all') {
-        return this.todos
-      } else if (this.filter === 'active') {
-        return this.todos.filter(todo => !todo.completed)
-      } else if (this.filter === 'completed') {
-        return this.todos.filter(todo => todo.completed)
-      } 
-      return this.todo
+      if (this.filter === "all") {
+        return this.todos;
+      } else if (this.filter === "active") {
+        return this.todos.filter(todo => !todo.completed);
+      } else if (this.filter === "completed") {
+        return this.todos.filter(todo => todo.completed);
+      }
+      return this.todo;
     },
     showClearCompletedButton() {
-      return this.todos.filter(todo => todo.completed).length > 0
+      return this.todos.filter(todo => todo.completed).length > 0;
     }
   },
   directives: {
     focus: {
-      inserted: function (el) {
-        el.focus()
+      inserted: function(el) {
+        el.focus();
       }
     }
   },
   methods: {
     addTodo() {
-      if(this.newTodo.trim().length === 0) {
-        return
+      if (this.newTodo.trim().length === 0) {
+        return;
       }
       this.todos.push({
         id: this.idForTodo,
         title: this.newTodo,
         completed: false,
         editing: false
-      })
-      this.newTodo = '',
-      this.idForTodo++
+      });
+      (this.newTodo = ""), this.idForTodo++;
     },
     editTodo(todo) {
-      this.beforeEditCache = todo.title,
-      todo.editing = true
+      (this.beforeEditCache = todo.title), (todo.editing = true);
     },
     doneEdit(todo) {
-      if(todo.title.trim() === '') {
-        todo.title = this.beforeEditCache
+      if (todo.title.trim() === "") {
+        todo.title = this.beforeEditCache;
       }
-      todo.editing = false
+      todo.editing = false;
     },
     cancelEdit(todo) {
-      todo.title = this.beforeEditCache,
-      todo.editing = false
+      (todo.title = this.beforeEditCache), (todo.editing = false);
     },
     removeTodo(index) {
-      this.todos.splice(index, 1)
+      this.todos.splice(index, 1);
     },
     checkAllTodos() {
-      this.todos.forEach(todo => todo.completed = event.target.checked)
+      this.todos.forEach(todo => (todo.completed = event.target.checked));
     },
     clearCompleted() {
-      this.todos = this.todos.filter(todo => !todo.completed)
+      this.todos = this.todos.filter(todo => !todo.completed);
     }
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
-  @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
 
-  .todo-input {
-    width: 100%;
-    padding: 10px 18px;
-    font-size: 18px;
-    margin-bottom: 16px;
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
 
-    &:focus {
-      outline: 0;
-    }
+  &:focus {
+    outline: 0;
   }
+}
 
-  .todo-item {
-    margin-bottom: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    animation-duration: 0.5s;
-  }
+.todo-item {
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  animation-duration: 0.5s;
+}
 
-  .remove-item {
-    cursor: pointer;
-    margin-left: 14px;
-    &:hover {
-      color: black;
-    }
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
+  &:hover {
+    color: black;
   }
+}
 
-  .todo-item-left {
-    display: flex;
-    align-items: center;
-  }
+.todo-item-left {
+  display: flex;
+  align-items: center;
+}
 
-  .todo-item-label {
-    padding: 10px;
-    border: 1px solid white;
-    margin-left: 12px;
-  }
+.todo-item-label {
+  padding: 10px;
+  border: 1px solid white;
+  margin-left: 12px;
+}
 
-  .todo-item-edit {
-    font-size: 24px;
-    color: #2c3e50;
-    margin-left: 12px;
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ccc;
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    &:focus {
-      outline: none;
-    }
+.todo-item-edit {
+  font-size: 24px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  &:focus {
+    outline: none;
   }
+}
 
-  .completed {
-    text-decoration: line-through;
-    color: grey;
-  }
+.completed {
+  text-decoration: line-through;
+  color: grey;
+}
 
-  .extra-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-size: 16px;
-    border-top: 1px solid lightgrey;
-    padding-top: 14px;
-    margin-bottom: 14px;
-  }
+.extra-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  border-top: 1px solid lightgrey;
+  padding-top: 14px;
+  margin-bottom: 14px;
+}
 
-  button {
-    font-size: 14px;
-    background-color: white;
-    &:hover {
-      background: lightgreen;
-    }
-    &:focus {
-      outline: none;
-    }
-  }
-  .active {
+button {
+  font-size: 14px;
+  background-color: white;
+  &:hover {
     background: lightgreen;
   }
+  &:focus {
+    outline: none;
+  }
+}
+.active {
+  background: lightgreen;
+}
 
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .2s;
-  }
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
