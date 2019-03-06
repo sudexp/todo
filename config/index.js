3ae40a70b64bc4f00908747f7f9c607a9c34a084
sudexp <sudexp@icloud.com>
2019-03-06 23:51:31 +0200

    config/index.js modified

--- config/index.js
+++ config/index.js
@@ -1,15 +1,14 @@
-'use strict'
+'use strict';
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/todo/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -20,7 +19,6 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
     /**
      * Source Maps
      */
@@ -66,4 +64,4 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   }
-}
+};
